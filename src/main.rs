de3da19ce940665f8b5a2c5bff984c511f62456f
Ziothh <louisgiet.w@gmail.com>
2024-06-03 18:03:03 +0200

    feat: base board entity and empty sprite

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+#![allow(dead_code)]
+#![allow(unused_variables)]
+
 use bevy::prelude::*;
 
 fn main() {
@@ -9,5 +12,37 @@ fn main() {
             }),
             ..default()
         }))
+        .add_startup_systems((setup, Board::spawn))
         .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+}
+
+#[derive(Component)]
+struct Board {
+    size: u8,
+}
+
+impl Board {
+    pub const TILE_SIZE: f32 = 40.;
+
+    fn physical_size(&self) -> f32 {
+        return f32::from(self.size) * Board::TILE_SIZE;
+    }
+
+    fn spawn(mut commands: Commands) {
+        let board = Board { size: 4 };
+
+        commands
+            .spawn(SpriteBundle {
+                sprite: Sprite {
+                    custom_size: Some(Vec2::new(board.physical_size(), board.physical_size())),
+                    ..default()
+                },
+                ..default()
+            })
+            .insert(board);
+    }
 }
