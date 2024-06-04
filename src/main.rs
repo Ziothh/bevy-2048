3d38aad8c4ac078384d59b7344acbc6f012e3a90
Ziothh <louisgiet.w@gmail.com>
2024-06-04 08:25:08 +0200

    chore: refactored render size calculations

--- src/main.rs
+++ src/main.rs
@@ -38,8 +38,33 @@ impl Board {
         alpha: 1.,
     };
 
-    fn physical_size(&self) -> f32 {
-        return self.size as f32 * Board::TILE_SIZE + (self.size + 1) as f32 * Board::TILE_SPACING;
+    fn new(size: u8) -> Self {
+        return Self { size };
+    }
+
+    fn physical_size(&self) -> Vec2 {
+        let size =
+            self.size as f32 * Board::TILE_SIZE + (self.size + 1) as f32 * Board::TILE_SPACING;
+        return Vec2::new(size, size);
+    }
+
+    fn origin(&self) -> Vec2 {
+        let size = self.physical_size();
+
+        return Vec2::new(
+            -size.x / 2. + Board::TILE_SIZE / 2.,
+            -size.y / 2. + Board::TILE_SIZE / 2.,
+        );
+    }
+
+    fn cell_position_to_physical(&self, x: u8, y: u8) -> Vec2 {
+        // Offset to the bottom left corner of the board
+        let offset = -self.physical_size() / 2. + Board::TILE_SIZE / 2.;
+
+        return Vec2::new(
+            offset.x + x as f32 * Board::TILE_SIZE + (x + 1) as f32 * Board::TILE_SPACING,
+            offset.y + y as f32 * Board::TILE_SIZE + (y + 1) as f32 * Board::TILE_SPACING,
+        );
     }
 
     fn spawn(mut commands: Commands) {
@@ -50,7 +75,7 @@ impl Board {
         commands
             .spawn(SpriteBundle {
                 sprite: Sprite {
-                    custom_size: Some(Vec2::new(board.physical_size(), board.physical_size())),
+                    custom_size: Some(board.physical_size()),
                     color: Board::COLOR,
                     ..default()
                 },
@@ -58,21 +83,15 @@ impl Board {
             })
             .with_children(|builder| {
                 for (x, y) in (0..board.size).cartesian_product(0..board.size) {
+                    let pos = board.cell_position_to_physical(x, y);
+
                     builder.spawn(SpriteBundle {
                         sprite: Sprite {
                             color: Tile::PLACEHOLDER_COLOR,
                             custom_size: Some(Vec2::new(Board::TILE_SIZE, Board::TILE_SIZE)),
                             ..default()
                         },
-                        transform: Transform::from_xyz(
-                            offset
-                                + x as f32 * Board::TILE_SIZE
-                                + (x + 1) as f32 * Board::TILE_SPACING,
-                            offset
-                                + y as f32 * Board::TILE_SIZE
-                                + (y + 1) as f32 * Board::TILE_SPACING,
-                            1.,
-                        ),
+                        transform: Transform::from_xyz(pos.x, pos.y, 1.),
                         ..default()
                     });
                 }
